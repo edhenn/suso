ad36ca5196a89b8926a3c2cb138fd97b7d8866cd
Ed Henn <emhenn@dcn.org>
2014-04-24 22:00:17 -0700

    add tests for subscribed events

--- test/obj-test.js
+++ test/obj-test.js
@@ -1,7 +1,19 @@
-/*global describe, it, test, expect */
+/*global describe, it, test, expect, beforeEach */
+/*jslint plusplus: true */
 
 (function () {
 	"use strict";
+
+	describe("test namespace", function () {
+		it("is defined", function () {
+			expect(test).toBeDefined();
+		});
+
+		it("has an EventAware function", function () {
+			expect(test.EventAware).toBeDefined();
+			expect(typeof test.EventAware).toBe("function");
+		});
+	});
 
 	describe("EventAware and derived objects", function () {
 		var x = new test.EventAware();
@@ -10,7 +22,7 @@
 			expect(x.on).toBeDefined();
 			expect(typeof x.on).toBe("function");
 		});
-		
+
 		it("has a .off function", function () {
 			expect(x.off).toBeDefined();
 			expect(typeof x.off).toBe("function");
@@ -19,6 +31,35 @@
 		it("has a .trigger function", function () {
 			expect(x.trigger).toBeDefined();
 			expect(typeof x.trigger).toBe("function");
+		});
+	});
+
+	describe("subscribed event", function () {
+		var timesCalled, watched, notWatched;
+
+		beforeEach(function () {
+			timesCalled = 0;
+			watched = new test.EventAware();
+
+			watched.on("blink", function () {
+				timesCalled++;
+			});
+		});
+
+		it("executes callback function when a subscribed event is fired", function () {
+			watched.trigger("blink");
+			expect(timesCalled).toBe(1);
+		});
+
+		it("does not execute callback function when a non-subscribed event is fired", function () {
+			watched.trigger("blonk");
+			expect(timesCalled).toBe(0);
+		});
+
+		it("does not execute callback function when a subscribed event is subsequently unsubscribed", function () {
+			watched.off("blink");
+			watched.trigger("blink");
+			expect(timesCalled).toBe(0);
 		});
 	});
 }());
