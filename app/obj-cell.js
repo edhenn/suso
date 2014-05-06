7f28aa7fe5ea317e2f6d00b54db6d70a3c2a7315
Ed Henn <emhenn@dcn.org>
2014-05-05 21:45:41 -0700

    jsobj.Cell functions and minimal tests: .value, .setValue, .row, .setRow

--- app/obj-cell.js
+++ app/obj-cell.js
@@ -4,6 +4,25 @@
 (function (jsobj) {
 	"use strict";
 
-	jsobj.Cell = Object.create(new jsobj.EventAware());
+	jsobj.Cell = function () {
+		var val, myrow;
 
+		this.prototype = new jsobj.EventAware();
+
+		this.value = function () {
+			return val;
+		};
+
+		this.setValue = function (newValue) {
+			val = newValue;
+		};
+
+		this.row = function () {
+			return myrow;
+		};
+
+		this.setRow = function (row) {
+			myrow = row;
+		};
+	};
 }(jsobj));
--- test/cell-test.js
+++ test/cell-test.js
@@ -12,7 +12,50 @@
 		});
 
 		it("is a function", function () {
-			expect(typeof jsobj.Cell).toBe("object");
+			expect(typeof jsobj.Cell).toBe("function");
+		});
+	});
+
+	describe("Cell.value function", function () {
+		var x = new jsobj.Cell();
+
+		it("is a member", function () {
+			expect(x.value).toBeDefined();
+		});
+
+		it("is a function", function () {
+			expect(typeof x.value).toBe("function");
+		});
+
+		it("returns undefined before a value is set", function () {
+			expect(x.value()).not.toBeDefined();
+		});
+
+		it("returns value after a value is set", function () {
+			x.setValue(5);
+			expect(x.value()).toBe(5);
+		});
+	});
+
+	describe("Cell.setValue function", function () {
+		var x = new jsobj.Cell();
+
+		it("is a member", function () {
+			expect(x.setValue).toBeDefined();
+		});
+
+		it("is a function", function () {
+			expect(typeof x.setValue).toBe("function");
+		});
+
+		it("sets Cell.value when currently undefined", function () {
+			expect(x.value()).not.toBeDefined();
+			x.setValue(6);
+			expect(x.value()).toBe(6);
+		});
+
+		xit("throws an error when called if Cell.value already defined", function () {
+			expect(x.setValue(7)).toThrow();
 		});
 	});
 }());
