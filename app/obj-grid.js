c40f314f1d05f9fd656b8ff013f4efd8dfa414ba
Ed Henn <emhenn@dcn.org>
2014-05-09 06:59:39 -0700

    grid - fix arrays of blocks and rows; fix reference to the arrays when adding cells

--- app/obj-grid.js
+++ app/obj-grid.js
@@ -5,11 +5,12 @@
 	"use strict";
 
 	jsobj.Grid = function (seeds) {
-		var blocks,		// 9 blocks of 9 cells each
-			vrows,		// 9 vertical rows of 9 cells each
-			hrows,		// 9 horizontal rows of 9 cells each
+		var blocks = [],	// 9 blocks of 9 cells each
+			vrows = [],		// 9 vertical rows of 9 cells each
+			hrows = [],		// 9 horizontal rows of 9 cells each
 			i,
-			newCell;
+			newCell,
+			cellnums = "created cells: ";
 
 		// create 9 blocks, vrows, hrows
 		for (i = 0; i < 9; i++) {
@@ -21,13 +22,12 @@
 		// create 81 cells each tied to correct block, vrow, hrow
 		for (i = 0; i < 81; i++) {
 			newCell = new jsobj.Cell();
-			newCell.setRowH(Math.floor(i / 9));		// every 9 consecutive cells make an hrow
-			newCell.setRowV(i % 9);					// every 9th cell belongs to the same vrow
-			newCell.setBlock(Math.floor(i / 3) % 3 + Math.floor(i / 27) * 3);	// every 3rd set of 3 consecutive cells up to 9 make a block
+			newCell.setRowH(hrows[Math.floor(i / 9)]);		// every 9 consecutive cells make an hrow
+			newCell.setRowV(vrows[i % 9]);					// every 9th cell belongs to the same vrow
+			newCell.setBlock(blocks[Math.floor(i / 3) % 3 + Math.floor(i / 27) * 3]);	// every 3rd set of 3 consecutive cells up to 9 make a block
+			cellnums += newCell.id().toString() + ',';
 		}
-
-		// each cell subscribes to the update event of cells in its hrow, vrow, and block
-
+		console.log(cellnums);
 
 		this.vRow = function (index) {
 			return vrows[index];
