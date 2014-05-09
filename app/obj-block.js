333677419ac6dce16d85f75a2af4064896828395
Ed Henn <emhenn@dcn.org>
2014-05-09 06:53:09 -0700

    block and row +addCell function to track all cells and subscribe to their updates

--- app/obj-block.js
+++ app/obj-block.js
@@ -5,6 +5,18 @@
 	"use strict";
 
 	function Block() {
+		var cells = [], that = this;
+
+		this.addCell = function (cell) {
+			cells.push(cell);
+			cell.on("update", function () {
+				that.trigger("update", this.value());
+			});
+		};
+
+		this.cells = function () {
+			return cells;
+		};
 	}
 
 	Block.prototype = new jsobj.EventAware();
--- app/obj-row.js
+++ app/obj-row.js
@@ -5,6 +5,18 @@
 	"use strict";
 
 	function Row() {
+		var cells = [], that = this;
+
+		this.addCell = function (cell) {
+			cells.push(cell);
+			cell.on("update", function () {
+				that.trigger("update", this.value());
+			});
+		};
+
+		this.cells = function () {
+			return cells;
+		};
 	}
 
 	Row.prototype = new jsobj.EventAware();
