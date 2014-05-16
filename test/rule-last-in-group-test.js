35a785dbd0b59f79e05f60f31d344d66c8db8de0
Ed Henn <emhenn@dcn.org>
2014-05-16 06:03:31 -0700

    test for solved cell when restricted by 2 blocks and one space remaining

--- test/rule-last-in-group-test.js
+++ test/rule-last-in-group-test.js
@@ -37,20 +37,37 @@
 		});
 
 		it("solves for a given value when its 4 neighboring blocks each contain the value", function () {
-			var grid = new jsobj.Grid(), solved;		// --- --- -5-
-														// --- --- ---
-			grid.hRow(0).cells()[7].setValue(5);		// --- --- ---
-			grid.hRow(3).cells()[8].setValue(5);
-			grid.hRow(6).cells()[0].setValue(5);		// --- --- --5
-			grid.hRow(7).cells()[3].setValue(5);		// --- --- ---
-														// --- --- ---
-			jsobj.ruleLastInGroup(grid);
-														// 5-- --- ---
-			solved = listSolved(grid);					// --- 5-- ---
-			expect(solved.length).toBe(5);				// --- --- X--
-			expect(solved[4].rowH().name()).toBe('row 8');
-			expect(solved[4].rowV().name()).toBe('col 6');
+			var grid = new jsobj.Grid(), solved;			//    012 345 678
+
+			grid.hRow(0).cells()[7].setValue(5);			// 0  --- --- -5-
+			grid.hRow(3).cells()[8].setValue(5);			// 1  --- --- ---            
+			grid.hRow(6).cells()[0].setValue(5);			// 2  --- --- ---
+			grid.hRow(7).cells()[3].setValue(5);
+															// 3  --- --- --5
+			jsobj.ruleLastInGroup(grid);        			// 4  --- --- ---
+															// 5  --- --- ---
+			solved = listSolved(grid);
+			expect(solved.length).toBe(5);					// 6  5-- --- ---
+			expect(solved[4].rowH().name()).toBe('row 8');	// 7  --- 5-- ---
+			expect(solved[4].rowV().name()).toBe('col 6');	// 8  --- --- X--
 			expect(solved[4].value()).toBe(5);
+		});
+
+		it("solves for a given value when 2 neighbor blocks restrict it to one row with only one space left in the block", function () {
+			var grid = new jsobj.Grid(), solved;			//    012 345 678
+
+			grid.hRow(6).cells()[2].setValue(3);			// 0  --- --- ---
+			grid.hRow(7).cells()[4].setValue(3);			// 1  --- --- ---
+			grid.hRow(8).cells()[6].setValue(1);			// 2  --- --- ---
+			grid.hRow(8).cells()[7].setValue(2);
+															// 3  --- --- ---
+			jsobj.ruleLastInGroup(grid);					// 4  --- --- ---
+															// 5  --- --- ---
+			solved = listSolved(grid);
+			expect(solved.length).toBe(5);					// 6  --3 --- ---
+			expect(solved[4].rowH().name()).toBe('row 8');	// 7  --- -3- ---
+			expect(solved[4].rowV().name()).toBe('col 8');	// 8  --- --- 12X
+			expect(solved[4].value()).toBe(3);
 		});
 	});
 }());
