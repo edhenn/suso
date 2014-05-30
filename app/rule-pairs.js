460cc2a4721c026e6d1a81c9f04c6476a0bd4eb6
Ed Henn <emhenn@dcn.org>
2014-05-30 06:38:50 -0700

    pairs rule initial comments

--- app/rule-pairs.js
+++ app/rule-pairs.js
@@ -8,8 +8,21 @@
 		jsobj.rules = {};
 	}
 
+	// Pairs rule removes possible values from cells.
+	// It looks in rows, columns, and blocks for any 2 cells containing the same two possible remaining values.
+	// The two values can be removed from all other cells in that group.
 	jsobj.rules.pairs = function (grid) {
-		var progress = false;
+		var progress = false,
+			allGroups = grid.allGroups(),
+			groupnum,
+			group
+			pairs;
+
+		// Iterate through each row, column, and block looking for pairs
+		for (groupnum = 0; groupnum < allGroups.length; groupnum++) {
+			pairs = {};
+
+		}
 
 		// rules return boolean indicating whether they made any progress
 		return progress;
