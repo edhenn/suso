72f4c797c8f0a512c3cc681d5b40fb85e9476ef1
Ed Henn <emhenn@dcn.org>
2015-03-23 21:07:42 -0700

    add rule-triples copied from rule-pairs, with logic commented out

---
+++ app/rule-triples.js
@@ -0,0 +1,63 @@
+/*global jsobj */
+/*jslint plusplus: true */
+
+(function (jsobj) {
+	"use strict";
+
+	if (jsobj.rules === undefined) {
+		jsobj.rules = {};
+	}
+
+	// Triples rule removes possible values from cells.
+	// It looks in rows, columns, and blocks for any 3 cells containing the same three possible remaining values between them.
+	// The three values can be removed from all other cells in that group.
+	jsobj.rules.triples = function (grid) {
+		var progress = false,
+			allGroups = grid.allGroups(),	// rows, cols, blocks
+			groupnum,
+			group,
+			cellnum,
+			cell,
+			twoValueCells,
+			pairs,
+			pairindex,
+			pairnum,
+			removal1,
+			removal2,
+			removal3;
+/*
+		// Iterate through each row, column, and block looking for pairs
+		for (groupnum = 0; groupnum < allGroups.length; groupnum++) {
+			group = allGroups[groupnum];
+			pairs = {};
+			// find all cells with only 2 possible values
+			for (cellnum = 0; cellnum < 9; cellnum++) {
+				cell = group.cells()[cellnum];
+				if (cell.value() === undefined && cell.possibleValues().length === 2) {
+					pairindex = cell.possibleValues().join('');
+					if (pairs[pairindex] === undefined) {
+						pairs[pairindex] = [];
+					}
+					pairs[pairindex].push(cell);
+				}
+			}
+			// look through the found cells for ones that are pairs (two cells with the same two possible values)
+			for (twoValueCells in pairs) {
+				if (pairs.hasOwnProperty(twoValueCells) && pairs[twoValueCells].length === 2) {
+					// delete those possible values from other cells in the group
+					for (cellnum = 0; cellnum < 9; cellnum++) {
+						cell = group.cells()[cellnum];
+						if (cell !== pairs[twoValueCells][0] && cell !== pairs[twoValueCells][1]) {
+							removal1 = cell.removePossible(parseInt(twoValueCells.split('')[0], 10));
+							removal2 = cell.removePossible(parseInt(twoValueCells.split('')[1], 10));
+							progress = progress || removal1 || removal2;
+						}
+					}
+				}
+			}
+		}
+*/
+		// rules return boolean indicating whether they made any progress
+		return progress;
+	};
+}(jsobj));
---
+++ test/rule-triples-test.js
@@ -0,0 +1,98 @@
+/*global jsobj, describe, it, xit, expect, beforeEach */
+/*jslint plusplus: true */
+
+(function () {
+	"use strict";
+
+	describe("rule-triples member", function () {
+		it("exists in jsobj namespace", function () {
+			expect(jsobj.rules.triples).toBeDefined();
+		});
+
+		it("is a function", function () {
+			expect(typeof jsobj.rules.triples).toBe("function");
+		});
+	});
+
+	xdescribe("rule-pairs function", function () {
+		function listSolved(grid) {
+			var solved = [], row, cell, rowCells;
+			for (row = 0; row < 9; row++) {
+				rowCells = grid.hRow(row).cells();
+				for (cell = 0; cell < 9; cell++) {
+					if (rowCells[cell].value() !== undefined) {
+						solved.push(rowCells[cell]);
+					}
+				}
+			}
+			return solved;
+		}
+
+		function arraysAreEqual(a, b) {
+
+		}
+
+		it("does nothing to an empty grid", function () {
+			var grid = new jsobj.Grid(), result = null;
+
+			result = jsobj.rules.pairs(grid);
+
+			expect(listSolved(grid).length).toBe(0);
+		});
+
+		it("returns false when it does nothing", function () {
+			var grid = new jsobj.Grid(), result = null;
+
+			result = jsobj.rules.pairs(grid);
+
+			expect(result).toBe(false);
+		});
+
+		it("removes pairs of possible values from a row when two cells in row have same pair of possible values", function () {
+			var grid = new jsobj.Grid(),					//    012 345 678
+				progress, solved, poss;
+
+			grid.hRow(0).cells()[0].setValue(1);			// 0  123 --- ---
+			grid.hRow(0).cells()[1].setValue(2);			// 1  --6 --- ---  <-- this row should not have possible vals 4,5 in cells 4-9.
+			grid.hRow(0).cells()[2].setValue(3);			// 2  789 --- ---
+			grid.hRow(1).cells()[2].setValue(6);
+			grid.hRow(2).cells()[0].setValue(7);			// 3  --- --- ---
+			grid.hRow(2).cells()[1].setValue(8);			// 4  --- --- ---
+			grid.hRow(2).cells()[2].setValue(9);			// 5  --- --- ---
+
+			progress = jsobj.rules.pairs(grid);				// 6  --- --- ---
+															// 7  --- --- ---
+			expect(progress).toBe(true);					// 8  --- --- ---
+			expect(grid.hRow(1).cells()[0].possibleValues()).toEqual([4, 5]);
+			expect(grid.hRow(1).cells()[1].possibleValues()).toEqual([4, 5]);
+			expect(grid.hRow(1).cells()[3].possibleValues()).toEqual([1, 2, 3, 7, 8, 9]);
+			expect(grid.hRow(1).cells()[4].possibleValues()).toEqual([1, 2, 3, 7, 8, 9]);
+			expect(grid.hRow(1).cells()[5].possibleValues()).toEqual([1, 2, 3, 7, 8, 9]);
+			expect(grid.hRow(1).cells()[6].possibleValues()).toEqual([1, 2, 3, 7, 8, 9]);
+			expect(grid.hRow(1).cells()[7].possibleValues()).toEqual([1, 2, 3, 7, 8, 9]);
+			expect(grid.hRow(1).cells()[8].possibleValues()).toEqual([1, 2, 3, 7, 8, 9]);
+		});
+
+		it("does nothing when 2 different pairs of possible values exist in a group", function () {
+			var grid = new jsobj.Grid(),					//    012 345 678
+				progress, solved, poss;
+
+			grid.hRow(0).cells()[0].setValue(1);			// 0  123 --- ---
+			grid.hRow(0).cells()[1].setValue(2);			// 1  --- --- ---  <-- cells 0,1,2 should have 5/6, 4/6, 4/5 as poss values.  no pairs.
+			grid.hRow(0).cells()[2].setValue(3);			// 2  789 --- ---
+			grid.hRow(2).cells()[0].setValue(7);
+			grid.hRow(2).cells()[1].setValue(8);			// 3  456 --- ---
+			grid.hRow(2).cells()[2].setValue(9);			// 4  --- --- ---
+			grid.hRow(3).cells()[0].setValue(4);			// 5  --- --- ---
+			grid.hRow(3).cells()[1].setValue(5);
+			grid.hRow(3).cells()[2].setValue(6);			// 6  --- --- ---
+															// 7  --- --- ---
+			progress = jsobj.rules.pairs(grid);				// 8  --- --- ---
+
+			expect(progress).toBe(false);
+			expect(grid.hRow(1).cells()[0].possibleValues()).toEqual([5, 6]);
+			expect(grid.hRow(1).cells()[1].possibleValues()).toEqual([4, 6]);
+			expect(grid.hRow(1).cells()[2].possibleValues()).toEqual([4, 5]);
+		});
+	});
+}());
