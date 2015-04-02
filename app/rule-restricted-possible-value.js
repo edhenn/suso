0732654415dde28b158bc434a2cfbae9c1ef52b0
Ed Henn <emhenn@dcn.org>
2015-04-01 21:30:44 -0700

    restricted value rule - rewrite with .each/.where

--- app/rule-restricted-possible-value.js
+++ app/rule-restricted-possible-value.js
@@ -11,35 +11,22 @@
 	// sometimes called "number claiming" or "omission"
 	jsobj.rules.restrictedPossibleValue = function (grid) {
 		var houses = grid.allGroups(),
-			houseNum,
-			house,
-			houseCells,
-			possnum,
-			possval,
 			intersects,
 			blocks,
 			rows,
 			cols,
-			cellnum,
-			cell,
-			intersectCell,
-			rowOrCol,
 			progress = false;
 
 		// iterate every house's remaining values (row, col, block)
 		// looking for values that are restricted to one intersecting house
 		// (row or col w/ val in one block only; block w/ val in one row or col only)
 		// to remove the value from other cells in the intersecting house.
-		for (houseNum = 0; houseNum < houses.length; houseNum++) {
-			house = houses[houseNum];
+		houses.each(function (house) {
 			// iterate remaining possible values in this house
-			for (possnum = 0; possnum < house.possibleValues().length; possnum++) {
-				houseCells = house.cells();
-				possval = house.possibleValues()[possnum];
+			house.possibleValues().each(function (possval) {
 				intersects = [[], []];	// [blocks, empty] for rows/cols; [rows, cols] for blocks
 				// iterate cells in house, looking for possible value restricted to one intersecting house
-				for (cellnum = 0; cellnum < houseCells.length; cellnum++) {
-					cell = houseCells[cellnum];
+				house.cells().each(function (cell) {
 					if (cell.possibles[possval] !== undefined) {
 						if (house.type() === 'block') {
 							rows = intersects[0];
@@ -57,32 +44,31 @@
 							}
 						}
 					}
-				}
+				});
 				// if possible value is in only one intersecting house, remove it from other cells of the intersecting house.
 				if (house.type() === 'block') {
-					for (rowOrCol = 0; rowOrCol < 2; rowOrCol++) {
-						if (intersects[rowOrCol].length === 1) {
-							for (cellnum = 0; cellnum < 9; cellnum++) {
-								intersectCell = intersects[rowOrCol][0].cells()[cellnum];
-								if (intersectCell.block() !== house && intersectCell.possibles[possval] !== undefined) {
-									progress = intersectCell.removePossible(possval);
-								}
+					intersects.where(function (rowOrCol) {
+						return rowOrCol.length === 1;
+					}).each(function (rowOrCol) {
+						rowOrCol[0].cells().each(function (intersectCell) {
+							if (intersectCell.block() !== house && intersectCell.possibles[possval] !== undefined) {
+								progress = intersectCell.removePossible(possval);
 							}
-						}
-					}
+						});
+					});
 				} else {
 					if (intersects[0].length === 1) {
-						for (cellnum = 0; cellnum < 9; cellnum++) {
-							intersectCell = intersects[0][0].cells()[cellnum];
-							if (intersectCell.row() !== house && intersectCell.col() !== house &&
+						intersects[0][0].cells().each(function (intersectCell) {
+							if (intersectCell.row() !== house &&
+									intersectCell.col() !== house &&
 									intersectCell.possibles[possval] !== undefined) {
 								progress = intersectCell.removePossible(possval);
 							}
-						}
+						});
 					}
 				}
-			}
-		}
+			});
+		});
 
 		// rules return boolean indicating whether they made any progress
 		return progress;
