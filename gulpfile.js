81b02556210d227b24e66b9afc3f6b34494e09bc
Ed Henn <emhenn@dcn.org>
2015-04-07 19:12:57 -0700

    gulpfile working with lint, test (gulp-karma), concat

--- gulpfile.js
+++ gulpfile.js
@@ -2,46 +2,13 @@
 
 var gulp = require('gulp'),
 	eslint = require('gulp-eslint'),
-	//karma = require('karma').server,
 	karma = require('gulp-karma'),
-	//jasmine = require('gulp-jasmine-phantom'),
 	concat = require('gulp-concat'),
 	stripDebug = require('gulp-strip-debug'),
 	rename = require('gulp-rename'),
 	notify = require('gulp-notify'),
-	uglify = require('gulp-uglify');
-
-gulp.task('lint', function () {
-	return gulp.src(['./app/*.js'])
-		.pipe(eslint())
-		.pipe(eslint.format())
-		.pipe(eslint.failOnError());
-});
-
-gulp.task('test', ['lint'], function (done) {
-	return gulp.src(['./test/obj-test.js'])
-		.pipe(karma({
-			basePath: '.',
-			files: ['app/suso-namespace.js'],
-			configFile: './karma.conf.js',
-			action: 'run'
-		}))
-		.on('error', function (err) {
-			//throw err;
-		});
-
-/*	karma
-	karma.start({
-		basePath: '.',
-		files: ['app/suso-namespace.js'],
-		configfile: './karma.conf.js',
-		singleRun: true
-	}, done);
-*/
-});
-
-gulp.task('concat', ['lint', 'test'], function () {
-	var filesInOrder = [
+	uglify = require('gulp-uglify'),
+	appFilesInOrder = [
 		'app/suso-namespace.js',
 		'app/obj.js',
 		'app/obj-query.js',
@@ -50,9 +17,32 @@ gulp.task('concat', ['lint', 'test'], function () {
 		'app/obj-cell.js',
 		'app/rule-*.js',
 		'app/view-*.js'
-	];
+	],
+	testFiles = ['test/*.js'];
 
-	return gulp.src(filesInOrder)
+gulp.task('lint', function () {
+	return gulp.src(appFilesInOrder)
+		.pipe(eslint())
+		.pipe(eslint.format())
+		.pipe(eslint.failOnError());
+});
+
+gulp.task('test', ['lint'], function (done) {
+	var sourceAndTestFiles = appFilesInOrder.concat(testFiles);
+
+	// gulp-karma
+	return gulp.src(sourceAndTestFiles)
+		.pipe(karma({
+			configFile: './karma.conf.js',
+			action: 'run'
+		}))
+		.on('error', function (err) {
+			//throw err;
+		});
+});
+
+gulp.task('concat', ['lint', 'test'], function () {
+	return gulp.src(appFilesInOrder)
 		.pipe(concat('suso.js'))
 		.pipe(stripDebug())
 		.pipe(gulp.dest('build'))
