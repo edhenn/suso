e1b56f626a2fece2603b1cf51dbf5edb16affa09
Ed Henn <emhenn@dcn.org>
2015-04-24 21:39:06 -0700

    gulpfile updated with git-tag-version semver

--- gulpfile.js
+++ gulpfile.js
@@ -48,25 +48,8 @@ gulp.task('complex', ['test', 'lint'], function () {
 	plugins.plato.inspect(appFilesInOrder, outputDir, {}, callback);
 });
 
-gulp.task('bump', ['lint', 'test'], function () {
-	return gulp.src('./package.json')
-		.pipe(plugins.bump())
-		.pipe(gulp.dest('./'));
-});
-
-gulp.task('bump-minor', ['lint', 'test'], function () {
-	return gulp.src('./package.json')
-		.pipe(plugins.bump({ type: 'minor' }))
-		.pipe(gulp.dest('./'));
-});
-
-gulp.task('bump-major', ['lint', 'test'], function () {
-	return gulp.src('./package.json')
-		.pipe(plugins.bump({ type: 'major' }))
-		.pipe(gulp.dest('./'));
-});
-
-function runpackage() {
+// package normal and minified version in build/ directory
+gulp.task('package', ['lint', 'test', 'complex'], function () {
 	return gulp.src(appFilesInOrder)
 		.pipe(plugins.concat('suso.js'))
 		.pipe(plugins.stripDebug())
@@ -74,19 +57,36 @@ function runpackage() {
 		.pipe(plugins.rename({ suffix: '.min' }))
 		.pipe(plugins.uglify())
 		.pipe(gulp.dest('build'));
+});
+
+function incrementVersion(versionType) {
+    // get all the files to bump version in
+    return gulp.src(['./package.json', './bower.json'])
+		// bump the version number in those files
+		.pipe(plugins.bump({ type: versionType }))
+		// save it back to filesystem
+		.pipe(gulp.dest('./'))
+		// commit the changed version number
+		.pipe(plugins.git.commit('update package version (gulp-tag-version)'))
+		 // read only one file to get the version number
+		.pipe(plugins.filter('package.json'))
+		// **tag it in the repository**
+		.pipe(plugins.tag_version());
 }
 
-// package auto-bumps semver patch number; gulp bump-minor or bump-major manually as desired
-gulp.task('package', ['lint', 'test', 'complex', 'bump'], function () {
-	return runpackage();
+// package and bump semver patch number in npm and bower configs
+gulp.task('patch', ['package'], function () {
+	return incrementVersion('patch');
 });
 
-gulp.task('package-minor', ['lint', 'test', 'complex', 'bump-minor'], function () {
-	return runpackage();
+// package and bump semver feature number in npm and bower configs
+gulp.task('feature', ['package'], function () {
+	return incrementVersion('minor');
 });
 
-gulp.task('package-major', ['lint', 'test', 'complex', 'bump-major'], function () {
-	return runpackage();
+// package and bump semver release number in npm and bower configs
+gulp.task('release', ['package'], function () {
+	return incrementVersion('major');
 });
 
 gulp.task('default', ['lint', 'test'], function () {
